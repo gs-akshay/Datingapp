5d6d9e3525941e10883a22d63211f879843e5c8d
gs-akshay <akshaygs97@gmail.com>
2025-03-20 13:34:23 +0530

    Eighth

---
+++ client/src/app/_interceptors/jwt.interceptor.ts
@@ -0,0 +1,18 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+import { authGuard } from '../_guards/auth.guard';
+
+export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
+  const accountService = inject(AccountService);
+
+  if(accountService.currentUser())
+  {
+    req = req.clone({
+      setHeaders:{
+        Authorization: `Bearer ${accountService.currentUser()?.token}`
+      }
+    })
+  }
+  return next(req);
+};
