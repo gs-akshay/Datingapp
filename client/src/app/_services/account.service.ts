b142acb21545f5cd8237eaf2bf127e3b29e4715b
gs-akshay <akshaygs97@gmail.com>
2025-05-21 19:43:50 +0530

    end of section 11

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -17,8 +17,7 @@ export class AccountService {
     return this.http.post<User>(this.baseUrl + "account/login", model).pipe(
       map(user => {
         if(user){
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUser.set(user);
+         this.currentUser.set(user);
         }
 
       })
@@ -29,12 +28,15 @@ export class AccountService {
     return this.http.post<User>(this.baseUrl + "account/register", model).pipe(
       map(user => {
         if(user){
-          localStorage.setItem('user', JSON.stringify(user));
           this.currentUser.set(user);
         }
          return user;
       })
     )
+  }
+   setCurrentUser(user: User) {
+    localStorage.setItem('user', JSON.stringify(user));
+    this.currentUser.set(user);
   }
 
 logout()
--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,12 +1,14 @@
-import { HttpClient} from '@angular/common/http';
-import { Injectable, inject,  signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/member';
 import { of, tap } from 'rxjs';
+import { Photo } from '../_models/Photo';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class MembersService {
   private http = inject(HttpClient);
   baseUrl = environment.apiUrl;
@@ -20,16 +22,42 @@ export class MembersService {
 
   getMember(username: string) {
     const member = this.members().find(x => x.username === username);
-    if(member !== undefined) return of(member);
+    if (member !== undefined) return of(member);
 
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
   updateMember(member: Member) {
     return this.http.put(this.baseUrl + 'users', member).pipe(
-    tap(() => {
-      this.members.update(members => members.map(m => m.username === member.username ? member : m))
-    })
-  )
+      tap(() => {
+        this.members.update(members => members.map(m => m.username === member.username ? member : m))
+      })
+    )
+  }
+
+  setMainPhoto(photo: Photo) {
+    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photo.id, {}).pipe(
+      tap(() => {
+        this.members.update(members => members.map(m => {
+          if (m.photos.includes(photo)) {
+            m.photoUrl = photo.url
+          }
+          return m;
+        }))
+      })
+    )
+  }
+
+  deletePhoto(photo: Photo) {
+    return this.http.delete(this.baseUrl + 'users/delete-photo/' + photo.id).pipe(
+       tap(() => {
+        this.members.update(members => members.map(m => {
+          if (m.photos.includes(photo)) {
+            m.photos = m.photos.filter(x => x.id !== photo.id)
+          }
+          return m;
+        }))
+      })
+    )
   }
 }
